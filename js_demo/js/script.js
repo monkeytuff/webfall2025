26b0a41d2f99169f0fa962bc88cf7e37ad2deef5
monkeytuff <anush@therawats.com>
2025-11-06 14:41:43 -0800

    Add files via upload

---
+++ js_demo/js/script.js
@@ -0,0 +1,7 @@
+function askQuestion() {
+    var p=prompt('How much money does monkey need to slime dem ops');
+    if (p !=null) {
+        document.getElementById ('question').innerHTML='Nah twin ' +p+ ' dollars is not enough for monkey to slime dem ops'
+    }
+
+}
